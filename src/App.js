2cc22a04c455a0f3c7dc496302a34f271ccd5bc4
JingyiNiu <niu.jy1203@gmail.com>
2021-03-24 10:21:45 +1300

    implement redirect and user type

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 
 import "./App.css";
 
 import Header from "./components/header/header.component";
-
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndRegisterPage from "./pages/sign-in-and-register/sign-in-and-register";
@@ -48,15 +47,31 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndRegisterPage} />
+          {/*if currentUser property exist, redirect to homepage. 
+          if not, go to sign-in-and-register page}*/}
+          <Route
+            exact
+            path='/signin'
+            render={() =>
+              this.props.currentUser ? (
+                <Redirect to='/' />
+              ) : (
+                <SignInAndRegisterPage />
+              )
+            }
+          />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser,
+});
+
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
