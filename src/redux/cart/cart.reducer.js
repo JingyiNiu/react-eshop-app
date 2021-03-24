ffd47320f4c68e1c451caac66036784d01396ac7
JingyiNiu <niu.jy1203@gmail.com>
2021-03-24 21:26:06 +1300

    implement add item in cart reducer

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,15 +1,26 @@
 import CartActionTypes from "./cart.types";
 
+// set initial state
 const INITIAL_STATE = {
   hidden: true,
+  cartItem: [],
 };
 
+// set corresponding return value for each case
 const cartReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    // change hidden state to opposite
     case CartActionTypes.TOGGLE_CART_HIDDEN:
       return {
         ...state,
         hidden: !state.hidden,
+      };
+
+    // add payload to cartItem array
+    case CartActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        cartItem: [...state.cartItem, action.payload],
       };
     default:
       return state;
