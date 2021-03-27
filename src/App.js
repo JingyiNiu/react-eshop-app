7bdf577f7b730bc6a73b577a1d9754dc09b22d39
JingyiNiu <niu.jy1203@gmail.com>
2021-03-27 14:06:08 +1300

    create checkout page

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ import { createStructuredSelector } from "reselect";
 import "./App.css";
 
 import Header from "./components/header/header.component";
-import HomePage from "./pages/homepage/homepage.component";
-import ShopPage from "./pages/shop/shop.component";
+import HomePage from "./pages/homepage/homepage";
+import ShopPage from "./pages/shop/shop";
 import SignInAndRegisterPage from "./pages/sign-in-and-register/sign-in-and-register";
+import CheckoutPage from "./pages/checkout/checkout"
+
 import { auth, createUserProfileDocument } from "./firebase/firebase.config";
 import { setCurrentUser } from "./redux/user/user.actions";
 import { selectCurrentUser } from "./redux/user/user.selectors";
@@ -49,6 +51,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route exact path='/checkout' component={CheckoutPage} />
           {/*if currentUser property exist, redirect to homepage. 
           if not, go to sign-in-and-register page}*/}
           <Route
---
+++ src/pages/checkout/checkout.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import "./checkout.style.scss";
+
+const CheckoutPage = () => <div>checkout page</div>;
+
+export default CheckoutPage;
